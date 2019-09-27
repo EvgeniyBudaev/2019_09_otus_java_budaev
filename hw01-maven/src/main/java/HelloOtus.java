00b745b9df8226f262f9f7def6264b50a5b6cc66
EvgeniyBudaev <budaev.e@gmail.com>
2019-09-27 18:45:13 +0300

    hw01-maven

--- hw01-maven/src/main/java/HelloOtus.java
+++ hw01-maven/src/main/java/HelloOtus.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import static com.google.common.base.Preconditions.checkArgument;
 
 public class HelloOtus {
+    public static void main(String[] args) {
+        int count = 1;
+
+        checkArgument(count > 0, "Count should be greater than zero");
+
+
+    }
 }
