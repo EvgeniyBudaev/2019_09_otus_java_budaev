ae9dd6438ba147fca6dd86986f25a18cf59bfbe6
EvgeniyBudaev <budaev.e@gmail.com>
2019-09-29 14:25:37 +0300

    hw01-maven. Сделал структуру hw01-maven/src/main/java/ru.otus.homework01/HelloOtus.java

---
+++ hw01-maven/src/main/java/ru/otus/homework01/HelloOtus.java
@@ -0,0 +1,11 @@
+package ru.otus.homework01;
+
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+public class HelloOtus {
+    public static void main(String[] args) {
+        int count = 1;
+        checkArgument(count > 0, "Count should be greater than zero");
+    }
+}
